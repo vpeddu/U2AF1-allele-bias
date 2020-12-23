0a06f614b2993ccb0ad1a2faea00306b20b3a196
vikas peddu <vikas.peddu@gmail.com>
2020-12-22 20:28:27 -0800

    found new gene????

--- allele_bias/analyze_allele_bias.r
+++ allele_bias/analyze_allele_bias.r
@@ -1,82 +1,13 @@
-library(vcfR)
-library(VariantAnnotation)
-library(TVTB)
-library(svMisc)
+library(Rsamtools)
 library(ggplot2)
+library(reshape2)
+
+read_counts <- read.csv('/Users/vikas/Documents/UCSC/rotations/Brooks/rotation_project/U2AF1-allele-bias/allele_bias/u2af1_ratios.csv', col.names = c('sample', 'U2AF1', 'newgene'))
+melt(read_counts)
+
+read_counts$ratio <- read_counts$newgene / read_counts$U2AF1
 
 
-# read from command line
-args = commandArgs(trailingOnly=TRUE)
-
-setwd("/Users/vikas/Documents/UCSC/rotations/Brooks/rotation_project/U2AF1-allele-bias/allele_bias")
-
-# testing
-args = '/Users/vikas/Documents/UCSC/rotations/Brooks/rotation_project/U2AF1-allele-bias/opossum/output'
-
-#read in vcf files
-vcf_files = list.files(args[1], pattern = "*.vcf", full.names = TRUE)
-
-treatment<-c()
-replicate<-c()
-full_path<-c()
-for( i in vcf_files){ 
-  t = strsplit(basename(i),'_R')[[1]][1]
-  r = strsplit(basename(i),'_R')[[1]][2]
-  treatment<-append(treatment,t )
-  replicate<-append(replicate, strsplit(r,'[.]')[[1]][1])
-  full_path<-append(full_path,i)
-  }
-
-metadata_df<-data.frame(treatment,replicate,full_path)
-
-analyze_vcf <- function(vcfpath){ 
-  temp_vcf <- VariantAnnotation::readVcf(vcfpath)
-   fixedRules <- VcfFixedRules(exprs = list(
-     alleleBias = expression(FILTER == "alleleBias"),
-     qual20 = expression(QUAL >= 20)
-   ))
-   active(fixedRules)["qual20"] <- FALSE
-   validrecords <- summary(evalSeparately(fixedRules, temp_vcf))[1]
-   allelebias<-summary(evalSeparately(fixedRules, temp_vcf))[2]
-   
-   return(c(validrecords, allelebias))
-}
-
-
-metadata_df$totalrecords<-NA
-metadata_df$allelebiascount<-NA
-for (i in 1:nrow(metadata_df)){
-  progress(i)
-  results<- analyze_vcf(metadata_df$full_path[i])
-  metadata_df$totalrecords[i]<-results[1]
-  metadata_df$allelebiascount[i]<-results[2]
-}
-
-
-
-sd_allelebias_df<-metadata_df[which(!duplicated(metadata_df$treatment)),]
-sd_allelebias_df$replicate<-NULL
-sd_allelebias_df$full_path<-NULL
-sd_allelebias_df$median<-NA
-sd_allelebias_df$q1<-NA
-sd_allelebias_df$q3<-NA
-for(i in  unique(metadata_df$treatment)){ 
-  summary<-(summary(100 * (metadata_df$allelebiascount[metadata_df$treatment == i]) / metadata_df$totalrecords[metadata_df$treatment == i]))
-  match<-which(sd_allelebias_df$treatment == i)
-  sd_allelebias_df$median[match]<-summary[3]
-  sd_allelebias_df$q1[match]<-summary[1]
-  sd_allelebias_df$q3[match]<-summary[6]
-  }
-
-allelebiascountplot<-ggplot(sd_allelebias_df,aes(x = treatment, y = median, fill = treatment)) + 
-  geom_bar(stat = 'identity') + 
-  theme_minimal() + 
-  geom_errorbar( aes(x=treatment, ymin=q1, ymax=q3)) +
-  scale_y_continuous(limits = c(0,2)) + 
-  ylab("% of total records marked as allele bias") +
-  theme(axis.text.x = element_text(angle = 45, hjust=1, vjust = 1.2), legend.position = "none")
-
-allelebiascountplot
-
-
-
+ggplot(read_counts, aes( x = factor(sample, level = level_order), y = ratio)) + 
+  geom_histogram(stat = 'identity') + 
+  theme_classic() + 
--- bitscore_analysis.py
+++ bitscore_analysis.py
@@ -1,6 +1,81 @@
+import os
 import sys
+import glob
 import pysam 
+import mygene
+import matplotlib
+import numpy as np
+import pandas as pd
 
-filepath = '/Users/vikas/Documents/UCSC/rotations/Brooks/rotation_project/U2AF1-allele-bias/test_bams/wgEncodeUwRepliSeqBg02esG2AlnRep1.R1.bam'
+bam_files = glob.glob('/Users/vikas/mount/vpeddu/platypus_run/bigboi/hisat2/*.bam')
+#bam_filepath = '/Users/vikas/mount/vpeddu/platypus_run/bigboi/hisat2/F_S14_L002.sorted.bam'
+for bam_filepath in bam_files:
+    
+    bamfile = pysam.AlignmentFile(bam_filepath, "rb")
 
-bamfile = pysam.AlignmentFile(filepath, "rb")
+    # bed_filepath = '/Users/vikas/Documents/UCSC/rotations/Brooks/rotation_project/U2AF1-allele-bias/test_bams/hg38.sorted.bed.gz'
+
+    # bedfile = pysam.TabixFile(bed_filepath)
+
+    files = []
+    primary_u2af1 = []
+    primary_newgene = []
+
+
+    # region is U2AF1
+    region = bamfile.fetch("chr21",43092956,43108291)
+
+    read_dict = {'name':[],'bitscore':[], "bases_aligned": [] , "reference_start": [], "file":[]}
+    for read in region:
+        read_dict['name'].append(read.query_name)
+        read_dict['bases_aligned'].append(len(read.get_reference_positions()))
+        read_dict['reference_start'].append(read.reference_start)
+        #ref_sequence = read.get_reference_sequence()
+        read_dict['bitscore'].append(read.flag)
+        read_dict['file'].append(os.path.basename(bam_filepath).split('.')[0])
+        #read_dict[readname] = [num_aligned, reference_positions, bitscore]
+        # prints readnames of files with secondary alignments
+        # if bitscore & 256: 
+    #bedfile.get_reference_name('ENST00000459639')
+    #df = pd.DataFrame(list(zip(readname, bitscore, reference_positions)))
+    df = pd.DataFrame(read_dict)
+    #df
+    secondary_aligned = [(name) for (score,name) in zip(df['bitscore'], df['name']) if score & 256]
+    primary_aligned = [(name) for (score,name) in zip(df['bitscore'], df['name']) if score & 256 == 0]
+
+
+    print('number of secondary aligned:' , str(len(secondary_aligned)))
+    print('number of primary aligned:' , str(len(primary_aligned)))
+    df.loc[df['bitscore'] & 256] 
+
+    files.append(os.path.basename(bam_filepath).split('.')[0])
+    primary_u2af1.append(len(primary_aligned))
+
+
+    # weird new region
+    new_region = bamfile.fetch("chr21",6230889,6235889)
+    read_dict = {'name':[],'bitscore':[], "bases_aligned": [] , "reference_start": [], "file":[]}
+    for read in new_region:
+        read_dict['name'].append(read.query_name)
+        read_dict['bases_aligned'].append(len(read.get_reference_positions()))
+        read_dict['reference_start'].append(read.reference_start)
+        #ref_sequence = read.get_reference_sequence()
+        read_dict['bitscore'].append(read.flag)
+        read_dict['file'].append(os.path.basename(bam_filepath).split('.')[0])
+        #read_dict[readname] = [num_aligned, reference_positions, bitscore]
+        # prints readnames of files with secondary alignments
+        # if bitscore & 256: 
+    #bedfile.get_reference_name('ENST00000459639')
+    #df = pd.DataFrame(list(zip(readname, bitscore, reference_positions)))
+    newdf = pd.DataFrame(read_dict)
+    newdf = newdf.loc[newdf['bitscore'] & 256 == 0] 
+    newdf
+
+    #files.append(os.path.basename(bam_filepath).split('.')[0])
+    primary_newgene.append(len(newdf))
+
+
+plot_df = pd.DataFrame(list(zip(primary_u2af1,primary_newgene)), index = files, columns = ['U2AF1',"New"])
+#plot_df = plot_df.T
+plot_df.plot.bar()
+plot_df
